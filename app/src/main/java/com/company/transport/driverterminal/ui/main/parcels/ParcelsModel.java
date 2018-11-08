2c4e1bc5c22408b5cfb1dfc41467eab64eaa5d54
denisrang <d.rangulov@innopolis.ru>
2018-11-08 11:28:04 +0300

    Changed parcel model

--- app/src/main/java/com/company/transport/driverterminal/ui/main/parcels/ParcelsModel.java
+++ app/src/main/java/com/company/transport/driverterminal/ui/main/parcels/ParcelsModel.java
@@ -1,41 +1,47 @@
 package com.company.transport.driverterminal.ui.main.parcels;
 
+import android.os.SystemClock;
+
 import com.company.transport.driverterminal.transportCompanyApi.TransportCompanyApiClient;
 import com.company.transport.driverterminal.transportCompanyApi.parcelListResponse.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 
 import io.reactivex.Single;
 
 public class ParcelsModel implements ParcelsContract.Model {
-    private TransportCompanyApiClient api;
+    private TransportCompanyApiClient apiClient;
     private List<Parcel> parcels = new ArrayList<>();
 
     @Inject
-    ParcelsModel(TransportCompanyApiClient api) {
-        this.api = api;
+    ParcelsModel(TransportCompanyApiClient apiClient) {
+        this.apiClient = apiClient;
     }
 
     @Override
-    public Single<Integer> downloadParcels(@ParcelsType int parcelType) {
-        switch (parcelType){
-            case ParcelsType.INCOMING:
-                parcels.clear();
-                parcels.add(new Parcel(16, "1.2", "2.3", "1", "5", "Main street", "14:20", "open"));
-                parcels.add(new Parcel(17, "2", "2", "1.7", "7", "Pushkin street", "20:00", "open"));
-                parcels.add(new Parcel(18, "4", "0.4", "2", "4", "Lermontov street", "8:00", "open"));
-            break;
-            case ParcelsType.COMPLETED:
-                parcels.clear();
-                parcels.add(new Parcel(3, "1.2", "2.3", "1", "0.45", "Big street", "14:20", "open"));
-                parcels.add(new Parcel(5, "2", "2", "1.7", "7", "Red street", "20:00", "open"));
-                break;
-        }
-        return Single.just(parcels.size());
-//        return api.getParcelList()
+    public Single<Integer> downloadParcels(@ParcelsType int parcelType)  {
+        return Single.fromCallable(()->{
+            SystemClock.sleep(2000);
+            switch (parcelType){
+                case ParcelsType.INCOMING:
+                    parcels.clear();
+                    parcels.add(new Parcel(16, "1.2", "2.3", "1", "5", "Main street", "14:20", "open"));
+                    parcels.add(new Parcel(17, "2", "2", "1.7", "7", "Pushkin street", "20:00", "open"));
+                    parcels.add(new Parcel(18, "4", "0.4", "2", "4", "Lermontov street", "8:00", "open"));
+                    break;
+                case ParcelsType.COMPLETED:
+                    parcels.clear();
+                    parcels.add(new Parcel(3, "1.2", "2.3", "1", "0.45", "Big street", "14:20", "open"));
+                    parcels.add(new Parcel(5, "2", "2", "1.7", "7", "Red street", "20:00", "open"));
+                    break;
+            }
+            return parcels.size();
+        });
+//        return apiClient.getParcelList()
 //                .map(parcelList -> {
 //                    this.parcels.clear();
 //                    this.parcels.addAll(parcelList);
